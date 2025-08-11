92d2fe724fc055e71b005cda61a70f90665fbfe9
Paulo Henrique da Silva Santos <phenriquess241987@gmail.com>
2025-08-11 12:58:14 -0300

    Update app_validador.py

--- app_validador.py
+++ app_validador.py
@@ -44,3 +44,19 @@ if arquivo:
         st.success("🎉 Validação concluída!")
         resultado_df = pd.DataFrame(resultados)
         st.dataframe(resultado_df)
+
+#conectar ao banco de dados do neon.tech
+
+import streamlit as st
+import psycopg2
+
+# Conectar ao banco usando secrets
+conn = psycopg2.connect(st.secrets["database"]["url"])
+cursor = conn.cursor()
+
+# Teste simples
+cursor.execute("SELECT version();")
+versao = cursor.fetchone()
+st.write("Conectado ao banco Neon!")
+st.write("Versão do PostgreSQL:", versao)
+
